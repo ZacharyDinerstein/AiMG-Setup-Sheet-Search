268a9287f0bd444fbf4910de5aeaca583e7ec1ad
Zack Dinerstein <zackdhome@gmail.com>
2017-05-03 16:55:41 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* TO DO
+- Fix dots so they show up on all browsers
+
+
+
+
+
+
+
+
+
+NEXT
+- Build second page
+
+
+ICEBOX
+- adjust font sizes in header on mobile
+- Fix header spacing on mobile
+- Adjust input fields so they populate our PHP file
+
+*/
